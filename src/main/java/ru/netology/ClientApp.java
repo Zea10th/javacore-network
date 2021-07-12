abfc338dfb8b090a0d37fce5b1144bb4fa9dd8da
Alexander Biezbardis <mr.biezbardis@gmail.com>
2021-07-12 21:15:18 +0300

    Added realization

---
+++ src/main/java/ru/netology/ClientApp.java
@@ -0,0 +1,21 @@
+package ru.netology;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientApp {
+    public static void main(String[] args) throws IOException {
+        String host = "127.0.0.1";
+        int port = 8005;
+        try (Socket clientSocket = new Socket(host, port);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("CAHEK");
+            String resp = in.readLine();
+            System.out.println(resp);
+        }
+    }
+}
