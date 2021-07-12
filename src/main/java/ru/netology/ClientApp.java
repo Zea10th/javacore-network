5728fd44abcc29b55c3b54bc810f7d1045e8b7eb
Alexander Biezbardis <mr.biezbardis@gmail.com>
2021-07-12 21:44:09 +0300

    Added trinkets

--- src/main/java/ru/netology/ClientApp.java
+++ src/main/java/ru/netology/ClientApp.java
@@ -8,14 +8,27 @@ import java.net.Socket;
 
 public class ClientApp {
     public static void main(String[] args) throws IOException {
-        String host = "127.0.0.1";
+        String host = "netology.local";
         int port = 8005;
         try (Socket clientSocket = new Socket(host, port);
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-            out.println("CAHEK");
-            String resp = in.readLine();
-            System.out.println(resp);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            while (true) {
+                String response = in.readLine();
+                System.out.println("Server asks: " + response);
+                if (response.contains("name")) {
+                    out.println("Alexander");
+                    System.out.println("Sent \"Alexander\"");
+                } else if (response.contains("child")) {
+                    out.println("no");
+                    System.out.println("Sent \"no\"");
+                    break;
+                }
+            }
+            System.out.println(in.readLine());
+//            out.println("CAHEK");
+//            String resp = in.readLine();
+//            System.out.println(resp);
         }
     }
 }
--- src/main/java/ru/netology/ServerApp.java
+++ src/main/java/ru/netology/ServerApp.java
@@ -13,13 +13,27 @@ public class ServerApp {
         ServerSocket serverSocket = new ServerSocket(port); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
         Socket clientSocket = serverSocket.accept(); // ждем подключения
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            System.out.println("New connection accepted");
 
-        System.out.println("New connection accepted");
+            out.println("Write your name");
+            final String name = in.readLine();
+            System.out.println(name + " connected to server");
 
-        final String name = in.readLine();
-
-        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
-
+            while (true) {
+                out.println("Are you child? (yes/no)");
+                final String option = in.readLine();
+                if (option.equals("yes")) {
+                    out.println(String.format("Welcome to the kids area, %s! Let's play!", name));
+                    break;
+                } else if (option.equals("no")) {
+                    out.println(String.format(
+                            "Welcome to the adult zone, %s! Have a good rest, or a good working day!", name));
+                    break;
+                } else {
+                    out.println("Please write \"yes\" or \"no\"");
+                }
+            }
+        }
     }
 }
